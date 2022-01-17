8bdf9ee722c8323358d63526b84843cc34dd48bd
Bryan Pearson <peabryan95@gmail.com>
2022-01-16 23:56:15 -0500

    update scripts

--- core/describe.py
+++ core/describe.py
@@ -1,27 +1,12 @@
 import socket
 
-multicast_ip = "224.0.23.12"
+ip = "224.0.23.12"
 port = 3671
 
-control_ip = "172.26.33.226"
-control_port = 0
-
-
-description_request = "06100203000e"
+# Specifies HPAI control endpoint 224.0.23.12:3671
+payload = bytearray.fromhex("06100203000e0801e000170c0e57")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-s.connect((multicast_ip, port))
-control_port = hex(s.getsockname()[1])[2:]
-print(control_port)
-control_ip = "".join([hex(int(x))[2:] for x in control_ip.split(".")])
-
-hpai_control_endpoint = "0801" + control_ip + control_port
-
-payload = bytearray.fromhex(description_request + hpai_control_endpoint)
-
+s.connect((ip, port))
 s.send(payload)
-
-recv = s.recv(1024)
-print(recv)
 s.close()
--- core/discover.py
+++ core/discover.py
@@ -3,7 +3,7 @@ import socket
 ip = "224.0.23.12"
 port = 3671
 
-# Send Search Request to Discovery Endpoint 224.0.23.12:3671
+# Specifies HPAI discovery endpoint 224.0.23.12:3671
 payload = bytearray.fromhex("06100201000e0801e000170c0e57")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
