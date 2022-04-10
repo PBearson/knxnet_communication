4da543132055613e6fd34ec360354c85270ae30d
Bryan Pearson <peabryan95@gmail.com>
2022-04-10 12:39:38 -0400

    Update search request

--- core/discover.py
+++ core/discover.py
@@ -1,12 +1,31 @@
 import socket
 
-ip = "224.0.23.12"
+ip = "localhost"
 port = 3671
 
-# Specifies HPAI discovery endpoint 224.0.23.12:3671
-payload = bytearray.fromhex("06100201000e0801e000170c0e57")
+fixed_header = bytearray.fromhex("0610")
+service_id = bytearray.fromhex("0201")
+total_length = bytearray.fromhex("0000") # Update later
+kip_header = fixed_header + service_id + total_length
+
+hpai_discovery_length = bytearray.fromhex("08")
+hpai_discovery_protocol = bytearray.fromhex("01") # UDP
+hpai_discovery_address = bytearray.fromhex("e000170c") # Address
+hpai_discovery_port = bytearray.fromhex("0e57") # Port
+kip_hpai_discovery = hpai_discovery_length + hpai_discovery_protocol + hpai_discovery_address + hpai_discovery_port
+
+
+# Fix total length field
+total_length_int = len(kip_header + kip_hpai_discovery)
+total_length = bytearray.fromhex("%.4x" % total_length_int)
+
+# Fix header
+kip_header = fixed_header + service_id + total_length
+
+# Craft final payload
+kip_payload = kip_header + kip_hpai_discovery
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((ip, port))
-s.send(payload)
+s.send(kip_payload)
 s.close()
